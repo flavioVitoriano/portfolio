7b839d28cedefdc9d7f155d5dfb0244d1ff15bd6
fvitoriano <flavio.vitorianodev@gmail.com>
2020-09-29 17:28:37 -0300

    Updates

---
+++ precache-manifest.bf502174d0e057e3643d829d771fbdaa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d14102273838e3bdb46ff83641c89a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "08f7d4d7509e44f1d7a3",
+    "url": "/portfolio/static/css/2.91f82eac.chunk.css"
+  },
+  {
+    "revision": "04e8c86c6a152457b242",
+    "url": "/portfolio/static/css/main.1d37f04c.chunk.css"
+  },
+  {
+    "revision": "08f7d4d7509e44f1d7a3",
+    "url": "/portfolio/static/js/2.86bc6480.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.86bc6480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e8c86c6a152457b242",
+    "url": "/portfolio/static/js/main.a16fdcb3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f9df14e9f203ab09662d6121fa6d07c3",
+    "url": "/portfolio/static/media/back1.f9df14e9.gif"
+  },
+  {
+    "revision": "795ea9774c61434ac710408ba8626894",
+    "url": "/portfolio/static/media/email.795ea977.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "eeecb602b53ffc2cf50ea75336d0384b",
+    "url": "/portfolio/static/media/login.eeecb602.png"
+  },
+  {
+    "revision": "e5ac63e73c1794a4c67556b6766f7010",
+    "url": "/portfolio/static/media/profile.e5ac63e7.jpg"
+  },
+  {
+    "revision": "8f24f73b934acbc9dcc3d7a1802e86cc",
+    "url": "/portfolio/static/media/tela01.8f24f73b.png"
+  },
+  {
+    "revision": "bb3289055e622d972de4ab1dc78e1448",
+    "url": "/portfolio/static/media/tela02.bb328905.png"
+  },
+  {
+    "revision": "3678f0b0249e5c6aa345786d19745594",
+    "url": "/portfolio/static/media/tela03.3678f0b0.png"
+  },
+  {
+    "revision": "b423b82487ba83ce272e5f11d35c5753",
+    "url": "/portfolio/static/media/tela04.b423b824.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.66f9c4e9b12290208ca6ae2de58a2962.js"
+  "/portfolio/precache-manifest.bf502174d0e057e3643d829d771fbdaa.js"
 );
 
 self.addEventListener('message', (event) => {
