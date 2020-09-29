690d8c8703f228cf78d6e369b7dd8b9be1ba4837
fvitoriano <flavio.vitorianodev@gmail.com>
2020-09-29 20:49:40 -0300

    added new item to portfolio

--- src/containers/MyWork/data.js
+++ src/containers/MyWork/data.js
@@ -7,7 +7,9 @@ import sige1 from "../../assets/works/sige/cliente_list.png";
 import sige2 from "../../assets/works/sige/cliente_create.png";
 import sige3 from "../../assets/works/sige/orcamento_entrada.png";
 import sige4 from "../../assets/works/sige/produto_create.png";
-
+import live1 from "../../assets/works/live-reports/usuarios.png";
+import live2 from "../../assets/works/live-reports/organizacao.png";
+import live3 from "../../assets/works/live-reports/importacao.png";
 
 const works = [
   {
@@ -20,36 +22,36 @@ const works = [
       {
         src: login,
         altText: "login page",
-        caption: "Login",
-        header: "",
+        caption: "pagina de Login",
+        header: "Login",
         key: "1",
       },
       {
         src: tela1,
         altText: "tela01",
-        caption: "Home",
-        header: "",
+        caption: "Contém informações úteis ao gestor",
+        header: "Home",
         key: "2",
       },
       {
         src: tela2,
         altText: "tela02",
-        caption: "Home",
-        header: "",
+        caption: "Pagina relacionada a alunos (CRUD)",
+        header: "Alunos",
         key: "3",
       },
       {
         src: tela3,
         altText: "tela03",
-        caption: "Tela 3",
-        header: "",
+        caption: "Mostra as eletivas disponíveis",
+        header: "Eletivas",
         key: "4",
       },
       {
         src: tela4,
         altText: "tela04",
-        caption: "Tela 4",
-        header: "",
+        caption: "Página relacionanda a relatórios",
+        header: "Gestão",
         key: "5",
       },
     ],
@@ -58,35 +60,64 @@ const works = [
     title: "F WEB SIGE",
     thumbnail: sige1,
     description: "Um sige web focado para empresas automobilísticas, feito com as funções básicas de um SIGE, com os módulos de cadastro, entrada, saídas e relatórios. Melhorias estão em progresso",
-    used: ["Django", "Django-rest-framework", "react", "reactjs", "postgresql", "mobx", "material ui", "selenium"],
+    used: ["Django", "Django-rest-framework", "react", "reactjs", "postgresql", "mobx", "material ui", "selenium", "plop"],
       items: [
       {
         src: sige1,
         altText: "sige01",
         caption: "Lista de clientes",
-        header: "",
+        header: "Clientes",
         key: "1",
       },
       {
         src: sige2,
         altText: "sige02",
         caption: "Criação de clientes",
-        header: "",
+        header: "Clientes",
         key: "2",
       },
       {
         src: sige3,
         altText: "sige03",
         caption: "Criação de orçamentos",
-        header: "",
+        header: "Orçamentos",
         key: "3",
       },
       {
         src: sige4,
         altText: "sige04",
         caption: "Criação de produtos",
-        header: "",
+        header: "Produtos",
         key: "4",
+      },
+    ],
+  },
+  {
+    title: "Live Reports",
+    thumbnail: live1,
+    description: "Sistema SAAS feito para processar e informar o fluxo de trabalho em empresas que usam relógios de ponto. Foi o primeiro app ao qual participei que inclui um sistema de permissões complexo, com grupos e permissões individuais e coletivas." ,
+    used: ["Java", "spring boot", "react", "reactjs", "mysql", "redux", "redux sagas", "intl", "plop"],
+      items: [
+      {
+        src: live1,
+        altText: "live01",
+        caption: "Página de CRUD de usuários",
+        header: "Usuários",
+        key: "1",
+      },
+      {
+        src: live2,
+        altText: "live02",
+        caption: "Modifica as regras da organização do usuário",
+        header: "Organizações",
+        key: "2",
+      },
+      {
+        src: live3,
+        altText: "live03",
+        header: "Importações",
+        caption: "Página ao qual se realiza as importações dos dados",
+        key: "3",
       },
     ],
   },
