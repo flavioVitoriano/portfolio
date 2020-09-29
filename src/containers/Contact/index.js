291c62f82be983c765bf045922f7df79a426f8af
fvitoriano <flavio.vitorianodev@gmail.com>
2020-09-29 16:01:31 -0300

    create contact page

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,30 +1,37 @@
 ﻿import React from "react";
-import { Container } from "reactstrap";
-import { Element } from "react-scroll";
+import { Container, Row, Col } from "reactstrap";
+import github from "../../assets/img/github.svg";
+import linkedin from "../../assets/img/linkedin.svg";
+import email from "../../assets/img/email.svg";
 
 const Contact = () => {
   return (
-      <div id="contact-section" className="content">
-        <div>
-          <Container>
-            <h1 className=" display-3">Contato</h1>
-            <hr />
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              luctus metus massa, sit amet varius arcu vulputate eget. Class
-              aptent taciti sociosqu ad litora torquent per conubia nostra, per
-              inceptos himenaeos. Praesent condimentum molestie dui, id mollis
-              nunc posuere nec. Sed congue ante quis erat luctus porta. Donec
-              vel pellentesque lectus, ut convallis dolor. Phasellus ultricies
-              rutrum mauris, nec tempor risus gravida vel. Quisque congue ante
-              vitae euismod tincidunt. Duis nisi ex, tristique in nisi at,
-              tempus ornare metus. Pellentesque vestibulum purus at lectus
-              rhoncus interdum. Duis at libero euismod, viverra tellus a,
-              tristique justo.
-            </p>
-          </Container>
-        </div>
+    <div id="contact-section" className="content">
+      <div>
+        <Container>
+          <h1 className=" display-3">Contato</h1>
+          <hr />
+
+          <Row>
+            <Col>
+              <a href="https://www.github.com/flavioVitoriano">
+              <img alt="github logo" class="img" width={100} height={100} src={github} />
+              </a>
+            </Col>
+            <Col>
+              <a href="https://www.linkedin.com/in/fl%C3%A1vio-vitoriano-28b6a41b0/">
+              <img alt="linkedin logo" class="img" width={100} height={100} src={linkedin}></img>
+              </a>
+            </Col>
+            <Col>
+              <a href="mailto:flavio.vitorianodev@gmail.com">
+            <img alt="email logo" class="img" width={100} height={100} src={email}></img>
+            </a>
+          </Col>
+          </Row>
+        </Container>
       </div>
+    </div>
   );
 };
 
